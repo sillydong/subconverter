60abb2b49e9d7ccdb2487dd9e5ba4c48ea7001cc
sillydong <njutczd@gmail.com>
2025-05-08 22:23:11 +0800

    fix h

--- src/utils/string.h
+++ src/utils/string.h
@@ -16,6 +16,7 @@ using string_multimap = std::multimap<std::string, std::string>;
 using string_pair_array = std::vector<std::pair<std::string, std::string>>;
 
 std::vector<std::string> split(const std::string &s, const std::string &separator);
+std::vector<std::string> splitN(const std::string &s, const std::string &separator, const int &count);
 std::vector<std::string_view> split(std::string_view s, char separator);
 void split(std::vector<std::string_view> &result, std::string_view s, char separator);
 std::string join(const string_array &arr, const std::string &delimiter);
@@ -23,18 +24,19 @@ std::string join(const string_array &arr, const std::string &delimiter);
 template <typename InputIt>
 std::string join(InputIt first, InputIt last, const std::string &delimiter)
 {
-    if(first == last)
+    if (first == last)
         return "";
-    if(std::next(first) == last)
+    if (std::next(first) == last)
         return *first;
-    return std::accumulate(std::next(first), last, *first, [&](const std::string &a, const std::string &b) {return a + delimiter + b; });
+    return std::accumulate(std::next(first), last, *first, [&](const std::string &a, const std::string &b)
+                           { return a + delimiter + b; });
 }
 
 std::string getUrlArg(const std::string &url, const std::string &request);
 std::string getUrlArg(const string_multimap &args, const std::string &request);
 std::string replaceAllDistinct(std::string str, const std::string &old_value, const std::string &new_value);
-std::string trimOf(const std::string& str, char target, bool before = true, bool after = true);
-std::string trim(const std::string& str, bool before = true, bool after = true);
+std::string trimOf(const std::string &str, char target, bool before = true, bool after = true);
+std::string trim(const std::string &str, bool before = true, bool after = true);
 std::string trimQuote(const std::string &str, bool before = true, bool after = true);
 void trimSelfOf(std::string &str, char target, bool before = true, bool after = true);
 std::string trimWhitespace(const std::string &str, bool before = false, bool after = true);
@@ -84,10 +86,10 @@ inline bool endsWith(const std::string &hay, const std::string &needle)
 inline bool count_least(const std::string &hay, const char needle, size_t cnt)
 {
     string_size pos = hay.find(needle);
-    while(pos != std::string::npos)
+    while (pos != std::string::npos)
     {
         cnt--;
-        if(!cnt)
+        if (!cnt)
             return true;
         pos = hay.find(needle, pos + 1);
     }
@@ -103,14 +105,14 @@ template <typename T>
 concept Arithmetic = std::is_arithmetic_v<T>;
 
 template <typename OutType, typename InType>
-requires Arithmetic<OutType>
+    requires Arithmetic<OutType>
 inline OutType to_number(const InType &value, OutType def_value = 0)
 {
     OutType retval = 0;
     char c;
     std::stringstream ss;
     ss << value;
-    if(!(ss >> retval) || ss >> c)
+    if (!(ss >> retval) || ss >> c)
         return def_value;
     else
         return retval;
@@ -128,21 +130,24 @@ concept Insertable = requires(Container a, Element b) {
     { a.insert(b) } -> std::same_as<typename Container::iterator>;
 };
 
-template<typename Container, typename KeyType, typename ValueType>
-requires Insertable<Container, std::pair<std::string, ValueType>>
-void fillMap(Container& map, KeyType&& key, ValueType&& value) {
+template <typename Container, typename KeyType, typename ValueType>
+    requires Insertable<Container, std::pair<std::string, ValueType>>
+void fillMap(Container &map, KeyType &&key, ValueType &&value)
+{
     map.insert({std::string(std::forward<KeyType>(key)), std::forward<ValueType>(value)});
 }
 
-template<typename Container, typename KeyType, typename ValueType, typename... Args>
-requires Insertable<Container, std::pair<std::string, ValueType>>
-void fillMap(Container& map, KeyType&& key, ValueType&& value, Args&&... args) {
+template <typename Container, typename KeyType, typename ValueType, typename... Args>
+    requires Insertable<Container, std::pair<std::string, ValueType>>
+void fillMap(Container &map, KeyType &&key, ValueType &&value, Args &&...args)
+{
     map.insert({std::string(std::forward<KeyType>(key)), std::forward<ValueType>(value)});
     fillMap(map, std::forward<Args>(args)...);
 }
 
-template<typename KeyType, typename ValueType, typename... Args>
-std::multimap<std::string, ValueType> multiMapOf(KeyType&& key, ValueType&& value, Args&&... args) {
+template <typename KeyType, typename ValueType, typename... Args>
+std::multimap<std::string, ValueType> multiMapOf(KeyType &&key, ValueType &&value, Args &&...args)
+{
     std::multimap<std::string, ValueType> result;
     fillMap(result, std::forward<KeyType>(key), std::forward<ValueType>(value), std::forward<Args>(args)...);
     return result;
@@ -151,12 +156,13 @@ std::multimap<std::string, ValueType> multiMapOf(KeyType&& key, ValueType&& valu
 #ifndef HAVE_TO_STRING
 namespace std
 {
-template <typename T> std::string to_string(const T& n)
-{
-    std::ostringstream ss;
-    ss << n;
-    return ss.str();
-}
+    template <typename T>
+    std::string to_string(const T &n)
+    {
+        std::ostringstream ss;
+        ss << n;
+        return ss.str();
+    }
 }
 #endif // HAVE_TO_STRING
 
